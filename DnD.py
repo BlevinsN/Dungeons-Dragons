dcb12d97d6b4f457a244d2d628d1f0e95f8a1307
Nate Blevins <nathanc.blevins@gmail.com>
2021-09-24 23:15:01 -0500

    Function Update
    BLEVINS: Updated for equipment as well. Need to see depth of equipment for structure types

--- DnD.py
+++ DnD.py
@@ -2,11 +2,11 @@ import requests
 from Dnd_Structures import *
 from Dnd_Functions import *
 
-
-request = input("Request: ")
-while request != "Bye.":
-	r = requests.get('https://www.dnd5eapi.co/api/' + request)
+quick_search()
+#request = input("Request: ")
+#while request != "Bye.":
+#	r = requests.get('https://www.dnd5eapi.co/api/' + request)
 	#new_object = add_object(r.json())
-	if not class_race_check(request, r.json()):
-		print(r.text)
-	request = input("Another: ")
+#	if not class_race_check(request, r.json()):
+#		print(r.text)
+#	request = input("Another: ")
--- Dnd_Functions.py
+++ Dnd_Functions.py
@@ -1,4 +1,27 @@
 from Dnd_Structures import *
+import requests
+
+
+def quick_search():
+	quick_options = {'1':"classes",
+					 '2':"races",
+					 '3':"equipment-categories",
+					 '4':"custom search"}
+	print("QUICK SEARCH")
+	print("____________")
+	for key,value in quick_options.items():
+		print(key, " : ", value)
+	user_selection = input("Make a selection: ")
+	if user_selection != 4:
+		r = requests.get('https://www.dnd5eapi.co/api/' + quick_options[user_selection])
+		results_check(r.json())
+	else:
+		custom_search = input("Search: ")
+		r = requests.get('https://www.dnd5eapi.co/api/' + custom_search)
+
+
+
+
 def add_object(item):
 	new_item = html_object(item['name'],
 				item['desc'],
@@ -14,22 +37,28 @@ def add_object(item):
 				item['attack_type'])
 	return new_item
 
-def list_classes_races(results_dict):
+def list_results(results_dict):
 	item_dict = {}
 	for item in results_dict['results']:
 		item_dict.update({item['name']:item['url']})
 	return item_dict
 
 def get_details(item_dict):
-	print("FIXME")
+	print("Would you like to see more details about one of the items?")
+	user_selection = input("If not, respond 'No':" )
+	if user_selection != "No":
+		while user_selection != "No":
+			print('https://www.dnd5eapi.co' + item_dict[user_selection])
+			element_details = requests.get('https://www.dnd5eapi.co' + item_dict[user_selection])
+			print(element_details.text)
+			print("Would you like to see more details about one of the items?")
+			user_selection = input("If not, respond 'No':" )
 
 
-def class_race_check(request, results_dict):
-	if (request == "races") | (request == "classes"):
-		item_dict = list_classes_races(results_dict)
-		print("RESULTS")
-		print("_______")
-		for element in item_dict:
-			print(" - ", element)
-		return True
-
+def results_check(results_dict):
+	item_dict = list_results(results_dict)
+	print("RESULTS")
+	print("_______")
+	for element in item_dict:
+		print(" - ", element)
+	get_details(item_dict)
